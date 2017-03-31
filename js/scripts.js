a5aacdb92620cfa73e40301d9f03761f920a1ad8
James Y Lee <babun5209@gmail.com>
2017-03-31 16:35:12 -0700

    add more functions

--- js/scripts.js
+++ js/scripts.js
@@ -3,16 +3,39 @@ var gallonsToLiters = function(gallons) {
   return gallons / 0.26417;
 };
 
+var litersToGallons = function(liters) {
+  return liters * 0.26417;
+}
+
 
 //Front-end(user interface) logic
 $(document).ready(function() {
-  $("form#unitConversion").submit(function(event) {
+  $("form#gallonsToLiters").submit(function(event) {
     event.preventDefault();
 
     var gallonsInput = $("input#gallons").val();
+    var result = gallonsToLiters(gallonsInput).toFixed(4);
 
-    var result = gallonsToLiters(gallonsInput).toFixed(5);
+    $(".outputResult").text(result + "L");
+    $("blockquote").show();
+  });
 
-    $("#outputResult").text(result + "L");
+  $("form#litersToGallons").submit(function(event) {
+    event.preventDefault();
+
+    var inputLiters = $("input#liters").val();
+    var result = litersToGallons(inputLiters).toFixed(4);
+
+    $(".outputResult").text(result + "gal");
+    $("blockquote").show();
+  });
+
+  $("button#red").click(function() {
+    $("body").removeClass();
+    $("body").addClass("red-background");
+  });
+  $("button#yellow").click(function() {
+    $("body").removeClass();
+    $("body").addClass("yellow-background");
   });
 });
